8e193136d6cbeb0dee642d9d2f3d571c67239654
Ayushtiwari2002 <98650309+Ayushtiwari2002@users.noreply.github.com>
2022-06-21 12:36:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculate() {
+    var p = document.getElementById("result").value;
+    document.getElementById("result").value =  eval(p);
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
